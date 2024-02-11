89023dde5d2360e0f035516b21668b0752f65328
Gérard Blankenberg <102226766+GerryHorror@users.noreply.github.com>
2024-02-11 02:36:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+document.getElementById("convert-btn").addEventListener("click", function () {
+  let number = document.getElementById("number").value;
+  let outputText = validateAndConvert(number);
+  let resultContainer = document.getElementById("result-container");
+  let outputElement = document.getElementById("output");
+
+  outputElement.innerText = outputText;
+  resultContainer.style.display = outputText ? "block" : "none";
+});
+
+function validateAndConvert(num) {
+  num = parseInt(num, 10);
+  if (isNaN(num)) {
+    return "Please enter a valid number";
+  } else if (num <= 0) {
+    return "Please enter a number greater than or equal to 1";
+  } else if (num > 3999) {
+    return "Please enter a number less than or equal to 3999";
+  } else {
+    return convertToRoman(num);
+  }
+}
+
+function convertToRoman(num) {
+  const numeralCodes = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1],
+  ];
+
+  let result = "";
+  numeralCodes.forEach((pair) => {
+    while (num >= pair[1]) {
+      result += pair[0];
+      num -= pair[1];
+    }
+  });
+  return result;
+}
